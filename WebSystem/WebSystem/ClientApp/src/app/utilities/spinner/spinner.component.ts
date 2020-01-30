4cd6de7ec7e7460092777e3a8897617b588db3f1
andre <andrenimacion@gmail.com>
2020-01-30 10:15:19 -0500

    diseño de spiner

--- WebSystem/WebSystem/ClientApp/src/app/utilities/spinner/spinner.component.ts
+++ WebSystem/WebSystem/ClientApp/src/app/utilities/spinner/spinner.component.ts
@@ -10,6 +10,27 @@ export class SpinnerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //this.animateSpinner();
   }
+
+  //animateSpinner() {
+  //  let cA = document.getElementById('cA');
+  //  let cB = document.getElementById('cB');
+  //  let cC = document.getElementById('cC');
+
+  //  setTimeout(() => {
+  //    cA.style.animationName = "cAnimate ";
+  //    cC.style.animationName = "cCnimate ";
+
+  //    cA.style.transform = "translateY(0px)";
+  //    cC.style.transform = "translateY(0px)";
+  //  }, 300)
+  //  setTimeout(() => {
+  //    cB.style.animationName = 'cBnimate';
+  //    cB.style.transform = 'translateX(-40px)';
+  //  },400)
+
+  //}
+
 
 }
